14cff2e197184d0ebe3c21e1cf8115226ecdcee0
reiwang <wangli.rei@gmail.com>
2016-04-05 14:49:48 +0800

    Delete useless game mode

--- Source/SGame/SGGameMode.h
+++ Source/SGame/SGGameMode.h
@@ -3,8 +3,26 @@
 #pragma once
 
 #include "GameFramework/GameMode.h"
+#include "SGLinkLine.h"
+#include "SGGrid.h"
 
 #include "SGGameMode.generated.h"
+
+/** Types of tile base type. */
+UENUM()
+namespace ESGGameStatus
+{
+	enum Type
+	{
+		EGS_Init = 0,					// Initial value
+		EGS_GameStart = 1,				// Shield
+		ETT_Potion = 2,					// Potion
+		ETT_Coin = 3,					// Coin
+		ETT_Mana = 4,					// Mana
+		ETT_Arrow = 5,					// Arrow
+		ETT_Soldier = 6					// Soldier
+	};
+}
 
 /**
  * 
